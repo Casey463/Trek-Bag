9d3c0922e9b39e259a77b3081b103d2867cdd7c0
Casey463 <kfmconroy@gmail.com>
2024-09-05 17:07:52 -0700

    Sidebar structure and markup
    Added buttons, styling, and structure to the sidebar component

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,3 +1,19 @@
+import Button from "./Button";
+
 export default function ButtonGroup() {
-  return <div></div>;
+  const secondaryButtons = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Reset to initial",
+    "Remove all items",
+  ];
+  return (
+    <section className="button-group">
+      {secondaryButtons.map((buttonText) => (
+        <Button type="secondary" key={buttonText}>
+          {buttonText}
+        </Button>
+      ))}
+    </section>
+  );
 }
